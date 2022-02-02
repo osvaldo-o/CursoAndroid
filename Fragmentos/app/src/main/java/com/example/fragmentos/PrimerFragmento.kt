786960ba4dc017e05ca34e4839b2610a93e3ecd0
osvaldo <osvaldo.guerra.c008@gmail.com>
2022-02-02 11:52:49 -0600

    Fragmentos

---
+++ Fragmentos/app/src/main/java/com/example/fragmentos/PrimerFragmento.kt
@@ -0,0 +1,15 @@
+package com.example.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PrimerFragmento : Fragment(R.layout.fragment_primer_fragmento) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
